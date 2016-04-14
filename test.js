d551cbec668eaa3554580b0b0e8320178fa23fb1
Seth Lieberman <shefseth@gmail.com>
2016-04-14 10:54:50 -0400

    organized sounds

--- test.js
+++ test.js
@@ -1,43 +1,37 @@
 $( document ).ready(function() {
 
 
-var audio = new Audio('audio/808.mp3');
-var audio1 = new Audio('audio/beatnick.mp3');
-var audio2 = new Audio('audio/blast.mp3');
-var audio3 = new Audio('audio/cy0000.mp3');
-var audio4 = new Audio('audio/atnight.mp3');
-var audio5 = new Audio('audio/fun.mp3');
-var audio6 = new Audio('audio/resurrection.mp3');
-var audio7 = new Audio('audio/vizor.mp3');
-var audio8 = new Audio('audio/wee.mp3');
-var audio9 = new Audio('audio/downer.mp3');
-var audio9 = new Audio('audio/cb.mp3');
-var audio9 = new Audio('audio/ch.mp3');
-var audio9 = new Audio('audio/cl.mp3');
-var audio9 = new Audio('audio/cp.mp3');
-var audio9 = new Audio('audio/ma.mp3');
+var audio1 = new Audio('audio/808.mp3');
+var audio2 = new Audio('audio/beatnick.mp3');
+var audio3 = new Audio('audio/blast.mp3');
+var audio4 = new Audio('audio/synth.mp3')
+var audio5 = new Audio('audio/atnight.mp3');
+var audio6 = new Audio('audio/fun.mp3');
+var audio7 = new Audio('audio/resurrection.mp3');
+var audio8 = new Audio('audio/vizor.mp3');
+var audio9 = new Audio('audio/wee.mp3');
+var audio10 = new Audio('audio/downer.mp3');
+var audio11 = new Audio('audio/cb.mp3');
+var audio12 = new Audio('audio/ch.mp3');
+var audio13 = new Audio('audio/cl.mp3');
+var audio14 = new Audio('audio/cp.mp3');
+var audio15 = new Audio('audio/ma.mp3');
 
 
 // function pause(){
 // 	document.getElementById("stop").addEventListener("click", function(){
 // 	audio.pause();
 // });
-
-
-document.getElementById("play").addEventListener("click", function(){
-	audio.loop = true;
-	audio.play();
-});
 document.getElementById("stop").addEventListener("click", function(){
-	audio.pause();
-  
+	audio.currentTime = 0;
 });
+
 
 document.getElementById("play1").addEventListener("click", function(){
 	audio1.loop = true;
 	audio1.play();
 });
-document.getElementById("stop1").addEventListener("click", function(){
+document.getElementById("pause1").addEventListener("click", function(){
 	audio1.pause();
   
 });
@@ -46,7 +40,7 @@ document.getElementById("play2").addEventListener("click", function(){
 	audio2.loop = true;
 	audio2.play();
 });
-document.getElementById("stop2").addEventListener("click", function(){
+document.getElementById("pause2").addEventListener("click", function(){
 	audio2.pause();
   
 });
@@ -55,7 +49,7 @@ document.getElementById("play3").addEventListener("click", function(){
 	audio3.loop = true;
 	audio3.play();
 });
-document.getElementById("stop3").addEventListener("click", function(){
+document.getElementById("pause3").addEventListener("click", function(){
 	audio3.pause();
   
 });
@@ -64,24 +58,24 @@ document.getElementById("play4").addEventListener("click", function(){
 	audio4.loop = true;
 	audio4.play();
 });
-document.getElementById("stop4").addEventListener("click", function(){
+document.getElementById("pause4").addEventListener("click", function(){
 	audio4.pause();
+  
 });
 
 document.getElementById("play5").addEventListener("click", function(){
 	audio5.loop = true;
 	audio5.play();
 });
-document.getElementById("stop5").addEventListener("click", function(){
+document.getElementById("pause5").addEventListener("click", function(){
 	audio5.pause();
-  
 });
 
 document.getElementById("play6").addEventListener("click", function(){
 	audio6.loop = true;
 	audio6.play();
 });
-document.getElementById("stop6").addEventListener("click", function(){
+document.getElementById("pause6").addEventListener("click", function(){
 	audio6.pause();
   
 });
@@ -90,7 +84,7 @@ document.getElementById("play7").addEventListener("click", function(){
 	audio7.loop = true;
 	audio7.play();
 });
-document.getElementById("stop7").addEventListener("click", function(){
+document.getElementById("pause7").addEventListener("click", function(){
 	audio7.pause();
   
 });
@@ -99,7 +93,7 @@ document.getElementById("play8").addEventListener("click", function(){
 	audio8.loop = true;
 	audio8.play();
 });
-document.getElementById("stop8").addEventListener("click", function(){
+document.getElementById("pause8").addEventListener("click", function(){
 	audio8.pause();
   
 });
@@ -108,8 +102,60 @@ document.getElementById("play9").addEventListener("click", function(){
 	audio9.loop = true;
 	audio9.play();
 });
-document.getElementById("stop9").addEventListener("click", function(){
+document.getElementById("pause9").addEventListener("click", function(){
 	audio9.pause();
+  
+});
+
+document.getElementById("play10").addEventListener("click", function(){
+	audio10.loop = true;
+	audio10.play();
+});
+document.getElementById("pause10").addEventListener("click", function(){
+	audio10.pause();
+  
+});
+document.getElementById("play11").addEventListener("click", function(){
+	audio11.loop = true;
+	audio11.play();
+});
+document.getElementById("pause11").addEventListener("click", function(){
+	audio11.pause();
+  
+});
+document.getElementById("play12").addEventListener("click", function(){
+	audio12.loop = true;
+	audio12.play();
+});
+document.getElementById("pause12").addEventListener("click", function(){
+	audio12.pause();
+  
+});
+
+document.getElementById("play13").addEventListener("click", function(){
+	audio13.loop = true;
+	audio13.play();
+});
+document.getElementById("pause13").addEventListener("click", function(){
+	audio13.pause();
+  
+});
+
+document.getElementById("play14").addEventListener("click", function(){
+	audio14.loop = true;
+	audio14.play();
+});
+document.getElementById("pause14").addEventListener("click", function(){
+	audio14.pause();
+  
+});
+
+document.getElementById("play15").addEventListener("click", function(){
+	audio15.loop = true;
+	audio15.play();
+});
+document.getElementById("pause15").addEventListener("click", function(){
+	audio15.pause();
   
 });
 
